b8a2146f9e8c28a9d540c08efa518a0170dc2467
Timmo <contact@timmo.xyz>
2020-05-30 11:19:53 +0100

    test

--- lyric/__init__.py
+++ lyric/__init__.py
@@ -5,10 +5,11 @@ import os
 import time
 
 import asyncio
+from abc import ABC, abstractmethod
 from aiohttp import ClientSession, ClientResponse
 import urllib.parse
 
-from . import Auth, Location
+from . import Location
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -219,3 +220,49 @@ class lyricDevice(object):
         """Return User Defined Name."""
 
         return self.device.get("userDefinedDeviceName")
+
+
+class Auth(AbstractAuth):
+    def __init__(self, websession: ClientSession, host: str, token_manager):
+        """Initialize the auth."""
+        super().__init__(websession, host)
+        self.token_manager = token_manager
+
+    async def async_get_access_token(self) -> str:
+        """Return a valid access token."""
+        if self.token_manager.is_token_valid():
+            return self.token_manager.access_token
+
+        await self.token_manager.fetch_access_token()
+        await self.token_manager.save_access_token()
+
+        return self.token_manager.access_token
+
+
+class AbstractAuth(ABC):
+    """Abstract class to make authenticated requests."""
+
+    def __init__(self, websession: ClientSession, host: str):
+        """Initialize the auth."""
+        self.websession = websession
+        self.host = host
+
+    @abstractmethod
+    async def async_get_access_token(self) -> str:
+        """Return a valid access token."""
+
+    async def request(self, method, url, **kwargs) -> ClientResponse:
+        """Make a request."""
+        headers = kwargs.get("headers")
+
+        if headers is None:
+            headers = {}
+        else:
+            headers = dict(headers)
+
+        access_token = await self.async_get_access_token()
+        headers["authorization"] = f"Bearer {access_token}"
+
+        return await self.websession.request(
+            method, f"{self.host}/{url}", **kwargs, headers=headers,
+        )
