b92d7ea976f18bb7716c567c1bc4ef1b9452412e
Aidan Timson <contact@timmo.xyz>
2020-08-30 14:22:03 +0100

    Versoining, Formatting

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
 import io
 
 from setuptools import setup
 
+version = "2.0.0"
 
-version = '1.1.6'
-
-
-setup(name='python-lyric',
-      version=version,
-      description='Python API for talking to the '
-                  'Honeywell Lyric™ Thermostat',
-      long_description=io.open('README.rst', encoding='UTF-8').read(),
-      keywords='honeywell lyric thermostat',
-      author='Bram Kragten',
-      author_email='mail@bramkragten.nl',
-      url='https://github.com/bramkragten/python-lyric/',
-      packages=['lyric'],
-      install_requires=['requests>=1.0.0',
-                        'requests_oauthlib>=0.7.0']
-      )
+setup(
+    name="python-lyric",
+    version=version,
+    description="Python API for talking to the " "Honeywell Lyric™ Thermostat",
+    long_description=io.open("README.rst", encoding="UTF-8").read(),
+    keywords="honeywell lyric thermostat",
+    author="Bram Kragten",
+    author_email="mail@bramkragten.nl",
+    url="https://github.com/bramkragten/python-lyric",
+    packages=["lyric"],
+    install_requires=["aiohttp>=3.6.2"],
+)
